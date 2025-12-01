324878b0f4d09429dedbac7c3e2da30f4b56955d
awang <111633856+awangsuryarmdhn@users.noreply.github.com>
2025-12-01 14:08:22 +0700

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,31 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 // import { RouterModule } from '@angular/router';
 import { Dashboard } from './dashboard/dashboard';
-import { Login } from './login/login';
+import { Login, LoginComponent } from './login/login';
 import { Register } from './register/register';
 // import { AdminComponent } from './admin/admin.component';
 import { Admin } from './admin/admin';
 import { Dashboard2 } from './dashboard2/dashboard2';
 import { Dashboard3 } from './dashboard3/dashboard3';
 import { Mahasiswa } from './mahasiswa/mahasiswa';
+import { otentikasiGuard } from './otentikasi-guard';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
         { path: '',  redirectTo: 'login', pathMatch: 'full'},
-        { path: "dashboard", component: Dashboard },
+        { path: "dashboard", component: Dashboard, canActivate: [otentikasiGuard] },
         { path: "login", component: Login },
         { path: "register", component: Register },
         { path: "admin", component: Admin },
-        { path: "dashboard2", component: Dashboard2 },
-        { path: "dashboard3", component: Dashboard3 },
-        { path: "mahasiswa", component: Mahasiswa },
+        { path: "dashboard2", component: Dashboard2, canActivate: [otentikasiGuard] },
+        { path: "dashboard3", component: Dashboard3, canActivate: [otentikasiGuard] },
+        { path: "mahasiswa", component: Mahasiswa, canActivate: [otentikasiGuard] },
 
 ];
+
+// @NgModule({
+//         imports: [ RouterModule.forRoot(routes)],
+//         exports: [ RouterModule ]
+// })
+
+// export class AppRoutes {}
